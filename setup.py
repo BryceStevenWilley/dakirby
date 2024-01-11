150e13f4ce7686ec4042b5d9daff4bcd007658b1
Bryce Willey <Bryce.Steven.Willey@gmail.com>
2024-01-11 16:03:35 -0500

    Some more packaging stuffs

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 import setuptools
 
+# https://packaging.python.org/en/latest/guides/single-sourcing-package-version/#id1, number 4
+
+with open("VERSION") as version_file:
+  version = version_file.read().strip()
+
 setuptools.setup(
     name="dakirby",
-    version="0.0.1",
+    version=version,
 )
